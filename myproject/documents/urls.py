f65fe3311038705253838d4460c15d0617d25fb1
woohyun23 <parkwh0823@gmail.com>
2024-07-09 22:05:52 +0900

    feat: 파일 조회

--- myproject/documents/urls.py
+++ myproject/documents/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DocumentUploadView
+from .views import DocumentUploadView, DocumentRead
 
 urlpatterns = [
     # document 업로드 api
-    path('api/v1/documents/', DocumentUploadView.as_view(), name='document_upload')
+    path('api/v1/documents/', DocumentUploadView.as_view(), name='document_upload'),
+    path('document/<int:documentId>', DocumentRead.as_view(), name='document_read'),
 ]
