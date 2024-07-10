fdfdb3cee1d842282ce12f06000960bbba228014
82everywin <82everywin@gmail.com>
2024-07-11 04:27:39 +0900

    issue: API 엔드포인트 수정

--- myproject/documents/urls.py
+++ myproject/documents/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # document 업로드 api
     path('documents/', DocumentUploadView.as_view(), name='document_upload'),
     path('documents/<int:documentId>', DocumentReadView.as_view(), name='document_read'),
-    path('documents/<int:documentId>/change/', DocumentChangeView.as_view(), name='document_change'),
+    path('documents/<int:documentId>/change', DocumentChangeView.as_view(), name='document_change'),
     path('documents/<int:documentId>/access', DocumentAccessView.as_view(), name='document_access')
 ] 
