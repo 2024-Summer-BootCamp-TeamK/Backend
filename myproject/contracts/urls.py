2dab256d57b920e01cdb465c4e716cb514d600df
82everywin <82everywin@gmail.com>
2024-07-11 01:08:43 +0900

    refactor: 임시로 endPoint 수정

--- myproject/contracts/urls.py
+++ myproject/contracts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('contracts/<int:contractId>', ContractModifyView.as_view(), name='contract_modify'),
     path('contracts/<int:contractId>/result', UpdatedContractReadView.as_view(), name='get_modified_contract'),
     path('contracts', UploadView.as_view(), name='upload'),
-    path('contracts/<int:contractId>', ContractDetailView.as_view(), name='upload'),
+    path('contracts/<int:contractId>/test', ContractDetailView.as_view(), name='check_contract'),
 ]
