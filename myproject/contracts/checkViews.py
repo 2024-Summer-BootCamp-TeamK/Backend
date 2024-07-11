e59e05583d1125f45f7d392d7f6bd818d11b23dc
82everywin <82everywin@gmail.com>
2024-07-11 16:24:46 +0900

    refactor: 비동기 처리 함수 구현

--- myproject/contracts/checkViews.py
+++ myproject/contracts/checkViews.py
@@ -1,21 +1,17 @@
 import fitz
-from django.core.files.base import ContentFile
+from celery import chain
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
-from requests import HTTPError
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-
-from .serializers import ArticleSerializer
+from .tasks import contract_origin_save, html_extract_content, pdf_to_text_and_openai, pdf_to_html_task
 from .utils.pdfToHtml import pdf_to_html_with_pdfco
-from .models import Contract, Type
+from .models import Contract
 import uuid
-import requests
 import os
 import json
-from .utils.openAICall import analyze_contract
 
 
 class UploadView(APIView):
@@ -49,50 +45,25 @@ class UploadView(APIView):
         }
     )
     def post(self, request, *args, **kwargs):
-
         category = request.data.get('category')
         pdf_file = request.FILES.get('pdf_file')
 
         if not category or not pdf_file:
             return Response({'error': 'Category와 PDF파일은 필수로 입력해야 합니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
         try:
             contract = Contract(category=category)
+            contract.save()
 
             file_name = f'{uuid.uuid4()}.pdf'
 
-            # pdf 파일 S3에 업로드
-            contract.origin_url.save(file_name, ContentFile(pdf_file.read()))
-            contract.save()
-
-            # S3 url 가져오기
-            pdf_url = contract.origin_url.url
-
-            # pdf -> html 코드 변환
-            pdfco_api_key = os.getenv('PDFCO_API_KEY')
-            try:
-                html_content = pdf_to_html_with_pdfco(pdfco_api_key, pdf_url)
-            except HTTPError as http_err:
-                return Response({'error': f'HTTP error occurred: {str(http_err)}'},
-                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-            except Exception as err:
-                return Response({'error': f'Other error occurred: {str(err)}'},
-                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-            # html 코드 S3에 업로드
-            if html_content:
-                html_file_name = f'{uuid.uuid4()}.html'
-
-                contract.origin.save(html_file_name, ContentFile(html_content.encode('utf-8')))
-                contract.save()
-                print(contract.origin.url)
+            result = chain(
+                contract_origin_save.s(contract.id, file_name, pdf_file.read()),
+                pdf_to_html_task.s(),
+            ).apply_async()
 
             return Response({
-                # 'message': 'success upload files',
-                'contractId': contract.id,
-                # 'category': contract.category,
-                # 'pdf_url': contract.origin_url.url,
-                # 'html_url': contract.origin.url,
-                # 'extracted_text': uploaded_html_content,
+                'contractId': result
             }, status=status.HTTP_201_CREATED)
 
         except UnicodeDecodeError as e:
@@ -113,27 +84,40 @@ class ContractDetailView(APIView):
         ],
         responses={
             200: openapi.Response('Success the results of reviewing the Contract',
-                openapi.Schema(
-                    type=openapi.TYPE_OBJECT,
-                    properties={
-                        'contractId': openapi.Schema(type=openapi.TYPE_INTEGER, description='ID of the Contract'),
-                        'contract': openapi.Schema(type=openapi.TYPE_STRING, description='Contract HTML Code File'),
-                        'articles': openapi.Schema(type=openapi.TYPE_ARRAY,
-                                        items=openapi.Schema(type=openapi.TYPE_OBJECT,
-                                        properties={
-                                            'articleId': openapi.Schema(type=openapi.TYPE_INTEGER, description='ID of the Article'),
-                                            'sentence': openapi.Schema(type=openapi.TYPE_STRING, description='강조할 내용'),
-                                            'types': openapi.Schema(
-                                                type=openapi.TYPE_ARRAY,
-                                                items=openapi.Schema(type=openapi.TYPE_STRING),
-                                                description=' main(주요조항) | toxin(독소조항) | ambi(모호한 표현)',
-                                                ),
-                                            'description': openapi.Schema(type=openapi.TYPE_STRING, description='해당 내용이 강조된 근거'),
-                                            'law': openapi.Schema(type=openapi.TYPE_STRING, description='해당 내용과 관련있는 법 조항'),
-                                            'recommend': openapi.Schema(type=openapi.TYPE_STRING, description='해당 문장의 수정 제안한 내용')
-                                        }
-                    ))}
-            )),
+                                  openapi.Schema(
+                                      type=openapi.TYPE_OBJECT,
+                                      properties={
+                                          'contractId': openapi.Schema(type=openapi.TYPE_INTEGER,
+                                                                       description='ID of the Contract'),
+                                          'contract': openapi.Schema(type=openapi.TYPE_STRING,
+                                                                     description='Contract HTML Code File'),
+                                          'articles': openapi.Schema(type=openapi.TYPE_ARRAY,
+                                                                     items=openapi.Schema(type=openapi.TYPE_OBJECT,
+                                                                                          properties={
+                                                                                              'articleId': openapi.Schema(
+                                                                                                  type=openapi.TYPE_INTEGER,
+                                                                                                  description='ID of the Article'),
+                                                                                              'sentence': openapi.Schema(
+                                                                                                  type=openapi.TYPE_STRING,
+                                                                                                  description='강조할 내용'),
+                                                                                              'types': openapi.Schema(
+                                                                                                  type=openapi.TYPE_ARRAY,
+                                                                                                  items=openapi.Schema(
+                                                                                                      type=openapi.TYPE_STRING),
+                                                                                                  description=' main(주요조항) | toxin(독소조항) | ambi(모호한 표현)',
+                                                                                              ),
+                                                                                              'description': openapi.Schema(
+                                                                                                  type=openapi.TYPE_STRING,
+                                                                                                  description='해당 내용이 강조된 근거'),
+                                                                                              'law': openapi.Schema(
+                                                                                                  type=openapi.TYPE_STRING,
+                                                                                                  description='해당 내용과 관련있는 법 조항'),
+                                                                                              'recommend': openapi.Schema(
+                                                                                                  type=openapi.TYPE_STRING,
+                                                                                                  description='해당 문장의 수정 제안한 내용')
+                                                                                          }
+                                                                                          ))}
+                                  )),
             400: 'Invalid data with Error GPT Request(Article)',
             404: 'Contract Of PDF File does not exist OR Contract Of HTML File does not exist ',
             500: 'Error processing request '
@@ -147,55 +131,10 @@ class ContractDetailView(APIView):
             pdf_url = contract.origin_url.url
             html_url = contract.origin.url
 
-            # 텍스트 추출
-            html_response = requests.get(html_url)
-            html_response.raise_for_status()
-            uploaded_html_content = html_response.content.decode('utf-8')
+            # 텍스트 추출 (celery)
+            uploaded_html_content = html_extract_content(html_url)
 
-            response = requests.get(pdf_url)
-            pdf_content = response.content
-
-            # 페이지 텍스트 추출
-            pdf_document = fitz.open(stream=pdf_content, filetype='pdf')
-            extracted_text = ""
-            for page_num in range(pdf_document.page_count):
-                page = pdf_document.load_page(page_num)
-                extracted_text += page.get_text()
-
-            raw_result = analyze_contract(extracted_text)
-
-            # 검토 결과 JSON 형태로 변경
-            parsed_result = json.loads(raw_result)
-            articles = []
-            for i in range(len(parsed_result)):
-                article_data = {
-                    "contract_id": contract.id,
-                    "sentence": parsed_result[i].get("sentence", ""),
-                    "description": parsed_result[i].get("description", ""),
-                    "law": parsed_result[i].get("law", ""),
-                    "recommend": parsed_result[i].get("recommend", "")
-                }
-                # 시리얼라이저를 이용해 데이터 저장
-                serializer = ArticleSerializer(data=article_data)
-
-                if serializer.is_valid():
-                    article_instance = serializer.save()
-
-                    for type_name in parsed_result[i]["types"]:
-                         type_instance = Type.objects.get(name=type_name)
-                         article_instance.type.add(type_instance)
-
-                    article_data = {
-                        "articleId": article_instance.id,
-                        "sentence": article_instance.sentence,
-                        "types": parsed_result[i].get("types", []),
-                        "description": article_instance.description,
-                        "law": article_instance.law,
-                        "recommend": article_instance.recommend
-                    }
-                    articles.append(article_data)
-                else:
-                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            articles = pdf_to_text_and_openai(contract, pdf_url)
 
             return Response({
                 'contractId': contract.id,
---
+++ myproject/contracts/tasks.py
@@ -0,0 +1,94 @@
+import json
+import uuid
+
+import fitz
+import os
+import requests
+from celery import shared_task
+from django.core.files.base import ContentFile
+from rest_framework.response import Response
+from rest_framework import status
+from contracts.models import Type, Contract
+from contracts.serializers import ArticleSerializer
+from contracts.utils.openAICall import analyze_contract
+from .utils.pdfToHtml import pdf_to_html_with_pdfco
+
+
+@shared_task()
+def contract_origin_save(contract_id, html_file_name, pdf_content):
+    contract = Contract.objects.get(id=contract_id)
+    contract.origin.save(html_file_name, ContentFile(pdf_content))
+    contract.save()
+    return contract.id
+
+@shared_task()
+def pdf_to_html_task(contract_id):
+    contract = Contract.objects.get(id=contract_id)
+    pdf_url = contract.origin_url.url
+
+    pdfco_api_key = os.getenv("PDFCO_API_KEY")
+    html_content = pdf_to_html_with_pdfco(pdfco_api_key, pdf_url)
+
+    if html_content:
+        html_file_name = f'{uuid.uuid4()}.html'
+
+        contract.origin.save(html_file_name, ContentFile(html_content.encode('utf-8')))
+        contract.save()
+    return contract.id
+
+
+@shared_task()
+def html_extract_content(html_url):
+    html_response = requests.get(html_url)
+    html_response.raise_for_status()
+    return html_response.content.decode('utf-8')
+
+
+@shared_task()
+def pdf_to_text_and_openai(contract, pdf_url):
+    response = requests.get(pdf_url)
+    pdf_content = response.content
+
+    # 페이지 텍스트 추출
+    pdf_document = fitz.open(stream=pdf_content, filetype='pdf')
+    extracted_text = ""
+    for page_num in range(pdf_document.page_count):
+        page = pdf_document.load_page(page_num)
+        extracted_text += page.get_text()
+
+    raw_result = analyze_contract(extracted_text)
+
+    # 검토 결과 JSON 형태로 변경
+    parsed_result = json.loads(raw_result)
+    articles = []
+    for i in range(len(parsed_result)):
+        article_data = {
+            "contract_id": contract.id,
+            "sentence": parsed_result[i].get("sentence", ""),
+            "description": parsed_result[i].get("description", ""),
+            "law": parsed_result[i].get("law", ""),
+            "recommend": parsed_result[i].get("recommend", "")
+        }
+        # 시리얼라이저를 이용해 데이터 저장
+        serializer = ArticleSerializer(data=article_data)
+
+        if serializer.is_valid():
+            article_instance = serializer.save()
+
+            for type_name in parsed_result[i]["types"]:
+                type_instance = Type.objects.get(name=type_name)
+                article_instance.type.add(type_instance)
+
+            article_data = {
+                "articleId": article_instance.id,
+                "sentence": article_instance.sentence,
+                "types": parsed_result[i].get("types", []),
+                "description": article_instance.description,
+                "law": article_instance.law,
+                "recommend": article_instance.recommend
+            }
+            articles.append(article_data)
+            return articles
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
