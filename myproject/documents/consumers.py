90fe802f66fecfe2bd110e936be0605fe6c1719f
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-07-14 21:48:17 +0900

    feat: pdf 스크롤 통신

--- myproject/documents/consumers.py
+++ myproject/documents/consumers.py
@@ -1,39 +1,26 @@
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
 
-class DocumentConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.document_id = self.scope['url_route']['kwargs']['document_id']
-        self.document_group_name = f'document_{self.document_id}'
+class PDFViewerConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
 
-        await self.channel_layer.group_add(
-            self.document_group_name,
-            self.channel_name
-        )
+    def disconnect(self, close_code):
+        pass
 
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
-            self.document_group_name,
-            self.channel_name
-        )
-
-    async def receive(self, text_data):
+    def receive(self, text_data):
         data = json.loads(text_data)
-        pointer_position = data['pointer_position']
-
-        await self.channel_layer.group_send(
-            self.document_group_name,
+        self.send(text_data=json.dumps(data))
+        self.channel_layer.group_send(
+            "pdf_viewer_group",
             {
-                'type': 'pointer_position',
-                'pointer_position': pointer_position
+                "type": "pdf_viewer_message",
+                "message": data
             }
         )
 
-    async def pointer_position(self, event):
-        pointer_position = event['pointer_position']
-
-        await self.send(text_data=json.dumps({
-            'pointer_position': pointer_position
+    def pdf_viewer_message(self, event):
+        message = event['message']
+        self.send(text_data=json.dumps({
+            'message': message
         }))
