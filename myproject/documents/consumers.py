4dcaa95d58c9b30f53f7089747f6b2e3cba7e656
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-07-15 15:23:08 +0900

    fix: 소켓 통신 수정

--- myproject/documents/consumers.py
+++ myproject/documents/consumers.py
@@ -1,11 +1,14 @@
+# documents/consumers.py
+
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class DocumentConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.document_id = self.scope['url_route']['kwargs']['documentId']
+        self.document_id = self.scope['url_route']['kwargs']['document_id']
         self.document_group_name = f'document_{self.document_id}'
 
+        # Join document group
         await self.channel_layer.group_add(
             self.document_group_name,
             self.channel_name
@@ -14,23 +17,33 @@ class DocumentConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
+        # Leave document group
         await self.channel_layer.group_discard(
             self.document_group_name,
             self.channel_name
         )
 
+    # Receive message from WebSocket
     async def receive(self, text_data):
         data = json.loads(text_data)
+        message_type = data['type']
+        payload = data['payload']
+
+        # Send message to document group
         await self.channel_layer.group_send(
             self.document_group_name,
             {
-                "type": "document_message",
-                "message": data
+                'type': f'document_{message_type}',
+                'payload': payload
             }
         )
 
-    async def document_message(self, event):
-        message = event['message']
+    # Receive message from document group
+    async def document_scroll(self, event):
+        payload = event['payload']
+
+        # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'type': 'scroll',
+            'payload': payload
         }))
