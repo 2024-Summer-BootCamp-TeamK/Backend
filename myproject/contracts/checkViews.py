987f4a252d480ea9124356ed4b3fdfe87dda9251
82everywin <82everywin@gmail.com>
2024-07-17 01:51:06 +0900

    feat: task_status 경로 변경

--- myproject/contracts/checkViews.py
+++ myproject/contracts/checkViews.py
@@ -132,3 +132,12 @@ class ContractDetailView(APIView):
         task_id = review_get_task.apply(args=[contractId])
         return Response({'task_id': task_id.id}, status=status.HTTP_200_OK)
 
+
+def task_status(request, task_id):
+    task = AsyncResult(task_id)
+    response = {
+        'task_id': task_id,
+        'status': task.status,
+        'result': task.result,
+    }
+    return Response(response)
--- myproject/contracts/urls.py
+++ myproject/contracts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .modifyViews import ContractModifyView, UpdatedContractReadView
-from .checkViews import UploadView, ContractDetailView
-from .statusViews import TaskStatusView
+from .checkViews import UploadView, ContractDetailView, task_status
 
 
 urlpatterns = [
@@ -9,6 +8,6 @@ urlpatterns = [
     path('contracts/<int:contractId>/result', UpdatedContractReadView.as_view(), name='get_modified_contract'),
     path('contracts', UploadView.as_view(), name='upload'),
     path('contracts/<int:contractId>/main', ContractDetailView.as_view(), name='check_contract_main'),
-    path('task_status/<str:task_id>', TaskStatusView.as_view(), name='task_status'),
+    path('task_status/<str:task_id>', task_status, name='task_status'),
 
 ]
