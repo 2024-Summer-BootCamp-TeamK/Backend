ad9fb864d904627f4809ab6a5b87218326e9a05e
82everywin <82everywin@gmail.com>
2024-07-17 00:18:40 +0900

    feat: task API url 구현

--- myproject/contracts/urls.py
+++ myproject/contracts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .modifyViews import ContractModifyView, UpdatedContractReadView
 from .checkViews import UploadView, ContractDetailView
+from .statusViews import task_status
 
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path('contracts/<int:contractId>/result', UpdatedContractReadView.as_view(), name='get_modified_contract'),
     path('contracts', UploadView.as_view(), name='upload'),
     path('contracts/<int:contractId>/main', ContractDetailView.as_view(), name='check_contract_main'),
+    path('task_status/<str:task_id>', task_status, name='task_status'),
 ]
