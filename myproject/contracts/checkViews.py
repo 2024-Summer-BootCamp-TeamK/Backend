2287d25bbc941c27c7cf3890691b791d25a4344a
82everywin <82everywin@gmail.com>
2024-07-17 02:01:59 +0900

    feat: task_status 반환값 수정

--- myproject/contracts/checkViews.py
+++ myproject/contracts/checkViews.py
@@ -135,9 +135,9 @@ class ContractDetailView(APIView):
 
 def task_status(request, task_id):
     task = AsyncResult(task_id)
-    response = {
+    result = {
         'task_id': task_id,
         'status': task.status,
         'result': task.result,
     }
-    return Response(response)
+    return Response(result, status=status.HTTP_200_OK)
