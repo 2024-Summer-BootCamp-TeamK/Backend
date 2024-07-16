52a149eececb37b75dc66ffa30faa9f209217fe0
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-07-17 00:59:25 +0900

    feat: 독소조항 분석 api path 지정

--- myproject/contracts/urls.py
+++ myproject/contracts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .modifyViews import ContractModifyView, UpdatedContractReadView
-from .checkViews import UploadView, ContractDetailView
+from .checkViews import UploadView, ContractDetailView, ContractToxinView
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('contracts/<int:contractId>/result', UpdatedContractReadView.as_view(), name='get_modified_contract'),
     path('contracts', UploadView.as_view(), name='upload'),
     path('contracts/<int:contractId>/main', ContractDetailView.as_view(), name='check_contract_main'),
+    path('contracts/<int:contractId>/toxin', ContractToxinView.as_view(), name='check_contract_toxin'),
 ]
