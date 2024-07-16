2816a5a6a333eec3cbee50d5cd883cdf49df91e6
82everywin <82everywin@gmail.com>
2024-07-17 02:06:01 +0900

    feat: task_status 뷰보기

--- myproject/contracts/checkViews.py
+++ myproject/contracts/checkViews.py
@@ -132,12 +132,3 @@ class ContractDetailView(APIView):
         task_id = review_get_task.apply(args=[contractId])
         return Response({'task_id': task_id.id}, status=status.HTTP_200_OK)
 
-
-def task_status(request, task_id):
-    task = AsyncResult(task_id)
-    result = {
-        'task_id': task_id,
-        'status': task.status,
-        'result': task.result,
-    }
-    return Response(result, status=status.HTTP_200_OK)
--- myproject/contracts/urls.py
+++ myproject/contracts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .modifyViews import ContractModifyView, UpdatedContractReadView
-from .checkViews import UploadView, ContractDetailView, task_status
+from .checkViews import UploadView, ContractDetailView
+from .statusViews import TaskStatusView
 
 
 urlpatterns = [
@@ -8,6 +9,6 @@ urlpatterns = [
     path('contracts/<int:contractId>/result', UpdatedContractReadView.as_view(), name='get_modified_contract'),
     path('contracts', UploadView.as_view(), name='upload'),
     path('contracts/<int:contractId>/main', ContractDetailView.as_view(), name='check_contract_main'),
-    path('task_status/<str:task_id>', task_status, name='task_status'),
+    path('task_status/<str:task_id>', TaskStatusView.as_view(), name='task_status'),
 
 ]
