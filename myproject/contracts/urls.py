102e3e174bb2127f2140ca445cde730b7d5822e1
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-07-17 17:34:12 +0900

    feat: 독소조항 분석 뷰 경로 할당

--- myproject/contracts/urls.py
+++ myproject/contracts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .modifyViews import ContractModifyView, UpdatedContractReadView
-from .checkViews import UploadView, ContractDetailView
+from .checkViews import UploadView, ContractMainView, ContractToxinView
 from .statusViews import TaskStatusView
 
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('contracts/<int:contractId>', ContractModifyView.as_view(), name='contract_modify'),
     path('contracts/<int:contractId>/result', UpdatedContractReadView.as_view(), name='get_modified_contract'),
     path('contracts', UploadView.as_view(), name='upload'),
-    path('contracts/<int:contractId>/main', ContractDetailView.as_view(), name='check_contract_main'),
+    path('contracts/<int:contractId>/main', ContractMainView.as_view(), name='check_contract_main'),
+    path('contracts/<int:contractId>/toxin', ContractToxinView.as_view(), name='check_contract_toxin'),
     path('task_status/<str:task_id>', TaskStatusView.as_view(), name='task_status'),
 ]
