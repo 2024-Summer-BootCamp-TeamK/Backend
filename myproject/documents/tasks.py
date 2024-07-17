b0c6629c60f9c81592caafeb7eb0e2ef870980d5
woohyun23 <parkwh0823@gmail.com>
2024-07-17 15:58:44 +0900

    feat: celery beat 적용 로그 추가

--- myproject/documents/tasks.py
+++ myproject/documents/tasks.py
@@ -24,6 +24,10 @@ def upload_file_to_s3(bucket_name, pdf_key, file_data):
   except Exception as e:
     return {'status': 'failed', 'error': str(e)}
 
+
+import logging
+
+logger = logging.getLogger(__name__)
 @shared_task
 def delete_expired_files():
     # S3 클라이언트 초기화
@@ -35,11 +39,18 @@ def delete_expired_files():
     # 만료된 파일을 찾기 위한 쿼리 작성
     expired_documents = Document.objects.filter(updatedAt__lt=expiration_date)
 
+    # 로그: 만료된 문서 수와 각 문서의 정보 출력
+    logger.debug(f"Found {expired_documents.count()} expired documents.")
     for document in expired_documents:
+        logger.debug(f"Expired document id={document.id}, pdfUrl={document.pdfUrl.name}")
+
         try:
             # S3에서 파일 삭제
-            s3.delete_object(Bucket='lawbotttt', Key=document.pdfUrl.name)
+            response = s3.delete_object(Bucket='lawbotttt', Key=document.pdfUrl.name)
+            logger.info(f"Deleted file {document.pdfUrl.name} from S3. Response: {response}")
+
             # 데이터베이스에서 해당 Document 삭제
             document.delete()
+            logger.info(f"Deleted document {document.id} from database.")
         except Exception as e:
-            print(f"Failed to delete {document.pdfUrl.name}: {e}")
+            logger.error(f"Failed to delete {document.pdfUrl.name}: {e}")
