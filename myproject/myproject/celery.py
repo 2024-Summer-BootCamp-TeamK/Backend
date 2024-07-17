b0f4ade03b79f4e6cc32e518333a641bbb0b6a06
woohyun23 <parkwh0823@gmail.com>
2024-07-17 15:58:44 +0900

    feat: celery beat 적용 로그 추가

--- myproject/myproject/celery.py
+++ myproject/myproject/celery.py
@@ -12,3 +12,19 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Django 앱에서 task를 자동으로 발견
 app.autodiscover_tasks()
+
+
+# celery.py 파일
+import logging
+
+# 기본 로그 설정
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# 콘솔 핸들러 설정
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
