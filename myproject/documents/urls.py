50e2c35dcfa7e774f2b8da21ce1b3647a5d6a2ad
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-07-19 22:42:35 +0900

    refactoring: 테스트 경로 추가

--- myproject/documents/urls.py
+++ myproject/documents/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import DocumentUploadView, DocumentView, DocumentAccessView
+from .encryptionView import DocumentEncryptionUploadView
 urlpatterns = [
     # document 업로드 api
     path('documents/', DocumentUploadView.as_view(), name='document_upload'),
     path('documents/<int:documentId>', DocumentView.as_view(), name='document_read'),
-    path('documents/<int:documentId>/access', DocumentAccessView.as_view(), name='document_access')
+    path('documents/<int:documentId>/access', DocumentAccessView.as_view(), name='document_access'),
+    path('encryption/test', DocumentEncryptionUploadView.as_view(), name='encryption_file_test')
 ] 
