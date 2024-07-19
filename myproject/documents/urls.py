96bd8acba818aebcf436aaa0ac6502bd7586c27c
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-07-20 02:57:34 +0900

    feat: 암호화 파일 조회 테스트

--- myproject/documents/urls.py
+++ myproject/documents/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import DocumentUploadView, DocumentView, DocumentAccessView
-from .encryptionView import DocumentEncryptionUploadView
+from .encryptionView import DocumentEncryptionUploadView, DocumentEncryptionView
 urlpatterns = [
     # document 업로드 api
     path('documents/', DocumentUploadView.as_view(), name='document_upload'),
     path('documents/<int:documentId>', DocumentView.as_view(), name='document_read'),
     path('documents/<int:documentId>/access', DocumentAccessView.as_view(), name='document_access'),
-    path('encryption/test', DocumentEncryptionUploadView.as_view(), name='encryption_file_test')
-] 
+    path('encryption/test', DocumentEncryptionUploadView.as_view(), name='encryption_file_test'),
+    path('encryption/test/<int:documentId>', DocumentEncryptionView.as_view(), name='encryption_file_view_test')
+]
