bb35ecad06e6b4ebeacb7d8f397bd06a29901267
서지민 <132264578+Lauiee@users.noreply.github.com>
2024-07-21 19:34:12 +0900

    issue: pdf_url안담기는 이슈 수정

--- myproject/documents/utils/customStorages.py
+++ myproject/documents/utils/customStorages.py
@@ -1,10 +1,8 @@
 # custom_storages.py
-import boto3
+
 from storages.backends.s3boto3 import S3Boto3Storage
 from base64 import b64encode
-from dotenv import load_dotenv
-import os
-load_dotenv()
+
 class CustomS3Boto3Storage(S3Boto3Storage):
     def _save(self, name, content):
         # 파일 저장
@@ -17,13 +15,7 @@ class CustomS3Boto3Storage(S3Boto3Storage):
         return name
 
     def _set_metadata(self, name, data_key_ciphertext):
-        # 지역을 명시하여 클라이언트 생성
-        s3_client = boto3.client(
-            's3',
-            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-            region_name=os.getenv('AWS_S3_REGION_NAME')
-        )
+        s3_client = self.connection.meta.client
         s3_client.copy_object(
             Bucket=self.bucket_name,
             Key=name,
