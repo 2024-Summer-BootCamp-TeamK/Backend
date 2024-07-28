10e4c8794ea5c157f342a7a5ab669c8b93fac0f1
82everywin <82everywin@gmail.com>
2024-07-28 23:50:19 +0900

    env: 경로 설정

--- myproject/start_celery_flower.sh
+++ myproject/start_celery_flower.sh
@@ -2,9 +2,8 @@
 
 # set two worker for testing purposes. 큐 하나당 워커 하나면 됩니다.
 
-celery -A backend worker --loglevel=info --concurrency=1 -n worker_1_@%h & # 워커 1번
-celery -A backend worker --loglevel=info --concurrency=1 -n worker_2_@%h & # 워커 2번
-celery -A backend worker --loglevel=info --concurrency=1 -n worker_3_@%h & # 워커 2번
+celery -A myproject worker --loglevel=info --concurrency=1 -n worker_1_@%h & # 워커 1번
+celery -A myproject worker --loglevel=info --concurrency=1 -n worker_2_@%h & # 워커 2번
+celery -A myproject worker --loglevel=info --concurrency=1 -n worker_3_@%h & # 워커 2번
 
-
-celery -A backend flower --port=5555 --basic_auth=guest:guest --broker=$CELERY_BROKER_URL --broker_api=$CELERY_BROKER_API_URL #플라워 시작
+celery -A myproject flower --port=5555 --basic_auth=guest:guest --broker=$CELERY_BROKER_URL --broker_api=$CELERY_BROKER_API_URL #플라워 시작
