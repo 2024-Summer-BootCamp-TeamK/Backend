58cda613443ace14a43fe6adcff3bfe57643a964
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-07-30 13:09:18 +0900

    Update serializers.py

--- myproject/documents/serializers.py
+++ myproject/documents/serializers.py
@@ -7,11 +7,8 @@ class DocumentSerializer(serializers.ModelSerializer):
         model = Document
         fields = ['id', 'email', 'pdfUrl']
 
-class DocumentUploadSerializer(serializers.Serializer):
+class EmailSerializer(serializers.Serializer):
     emails = serializers.ListField(
         child=serializers.EmailField(),
         help_text="이메일 주소 배열"
-    )
-    pdfFile = serializers.FileField(
-        help_text="PDF 파일"
     )
